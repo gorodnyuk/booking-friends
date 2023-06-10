abcedf4f1b976b96ebe51387fc38679c3a1fcfea
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-06-10 13:14:02 +0300

    Init commit

---
+++ src/main/java/pro/gorodnyuk/bookingfriends/BookingFriendsApplication.java
@@ -0,0 +1,13 @@
+package pro.gorodnyuk.bookingfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingFriendsApplication.class, args);
+	}
+
+}
