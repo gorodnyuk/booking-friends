c480937ea62368c13f77cbdf9bbe91ef743c5f20
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-09-23 15:09:59 +0300

    Доработана главная страница

--- src/main/java/pro/gorodnyuk/bookingfriends/web/BookingFriendsController.java
+++ src/main/java/pro/gorodnyuk/bookingfriends/web/BookingFriendsController.java
@@ -1,10 +1,21 @@
 package pro.gorodnyuk.bookingfriends.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import pro.gorodnyuk.bookingfriends.service.BookingFriendsService;
 
 @Controller
+@RequiredArgsConstructor
 public class BookingFriendsController {
+
+    private final BookingFriendsService service;
+
+    @GetMapping("/")
+    public String index() {
+        throw new RuntimeException("Not implemented yet");
+    }
 
     @PostMapping("/reserve")
     public String downloadCertificate() {
