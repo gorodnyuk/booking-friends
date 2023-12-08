033f3e0f199d1822b7e9b2c54e1b3607a70683b8
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-12-08 13:30:00 +0300

    Добавлены аннотации для работы с бд

--- src/main/java/pro/gorodnyuk/bookingfriends/entity/BookingFriend.java
+++ src/main/java/pro/gorodnyuk/bookingfriends/entity/BookingFriend.java
@@ -1,6 +1,32 @@
 package pro.gorodnyuk.bookingfriends.entity;
 
+import lombok.AllArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Entity
+@Table(name = "person")
 public class BookingFriend {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "middle_name", nullable = false)
+    private String middleName;
+
+    @Column(name = "booking_date", nullable = false)
+    private LocalDate bookingDate;
 }
