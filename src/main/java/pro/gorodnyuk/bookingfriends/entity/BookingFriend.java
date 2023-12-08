9922fe29f6174589d14884e5e8bdd2d8919911a4
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-12-08 14:01:17 +0300

    Добавлена миграция

--- src/main/java/pro/gorodnyuk/bookingfriends/entity/BookingFriend.java
+++ src/main/java/pro/gorodnyuk/bookingfriends/entity/BookingFriend.java
@@ -24,7 +24,7 @@ public class BookingFriend {
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "middle_name", nullable = false)
+    @Column(name = "middle_name")
     private String middleName;
 
     @Column(name = "booking_date", nullable = false)
